2762768c7ccf224c397cc7d4b6bf60a19d203cb4
Aryainguz <aryaninguz369@gmail.com>
2024-01-07 14:48:11 +0530

    added transaction page

---
+++ src/pages/AddProduct.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '../components/Sidebar'
+import NewProduct from '../components/NewProduct'
+
+const AddProduct = () => {
+  return (
+    <Sidebar>
+      <NewProduct/>
+    </Sidebar>
+  )
+}
+
+export default AddProduct
